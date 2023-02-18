cfabb2b057bfa4954dbd8140265f9120c06efdc5
imtanjil <tanjil4work@gmail.com>
2023-02-18 11:14:26 +0600

    header complete

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div>
-            this is header
+  const navItems = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact">Contact</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 shadow-md mb-5 rounded-b-md">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navItems}
+          </ul>
         </div>
-    );
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          JPT Task
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navItems}</ul>
+      </div>
+      <div className="navbar-end">
+        <div className="form-control">
+          <label className="label cursor-pointer">
+            <span className="label-text">Job Seeker/Poster </span>
+            <input type="checkbox" className="toggle ml-2" />
+          </label>
+        </div>
+        <Link to="/login" className="btn">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
